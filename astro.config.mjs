e131a8bd927e9d4dcbc0167404c2b19baf84a17f
ptrst102 <ptrst102@gmail.com>
2024-02-09 23:33:07 +0900

    icon追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
@@ -7,7 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ptrst102.com",
-  integrations: [tailwind(), sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react(), icon()],
   markdown: {
     remarkPlugins: ["remark-breaks"],
     extendDefaultPlugins: true,
