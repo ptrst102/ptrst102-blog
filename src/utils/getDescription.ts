9d2d5f3c04becd69c91c1746b40491a59de71b14
ptrst102 <ptrst102@gmail.com>
2024-02-12 05:32:42 +0900

    getDescription　リンクも除外するよう修正

--- src/utils/getDescription.ts
+++ src/utils/getDescription.ts
@@ -1,7 +1,11 @@
 import type { CollectionEntry } from "astro:content";
 
 export const getDescription = (post: CollectionEntry<"blog">) => {
-  const body = post.body.replace(/^#+\s/gm, "").replace(/\n/g, "");
+  const body = post.body
+    .replace(/^#+\s/gm, "") // ヘッダーを削除
+    .replace(/\n/g, "") // 改行を削除
+    .replace(/!\[.*?\]\(.*?\)/g, "") // 画像を削除
+    .replace(/\[(.*?)\]\(.*?\)/g, "$1"); // リンクのURLを除外し、テキストのみを残す
 
   const MAX_LENGTH = 100;
   return body.length > MAX_LENGTH
