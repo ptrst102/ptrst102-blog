e2575d6be593c94a0b2d435563ca32ae680e9f6c
ptrst102 <ptrst102@gmail.com>
2024-02-12 04:36:14 +0900

    remark-ogp-card追加

---
+++ tools/remark-ogp-card/index.js
@@ -0,0 +1,80 @@
+import ogs from "open-graph-scraper";
+import { visit } from "unist-util-visit";
+
+const ogpCardPlugin = () => {
+  return async (tree) => {
+    const transformers = [];
+    visit(tree, "paragraph", (paragraphNode, index) => {
+      if (paragraphNode.children.length !== 1) {
+        return tree;
+      }
+
+      if (paragraphNode && paragraphNode.data !== undefined) {
+        return tree;
+      }
+
+      visit(paragraphNode, "text", (textNode) => {
+        const urls = textNode.value.match(
+          /(https?:\/\/|www(?=\.))([-.\w]+)([^ \t\r\n]*)/g,
+        );
+
+        if (!urls || urls.length !== 1) {
+          return;
+        }
+
+        const url = urls[0];
+        transformers.push(async () => {
+          const { result } = await ogs({ url });
+
+          const cardNode = {
+            type: "html",
+            value: createCard(url, extractDomain(url), result.ogTitle),
+          };
+
+          tree.children.splice(index, 1, cardNode);
+        });
+      });
+    });
+
+    try {
+      await Promise.all(transformers.map((t) => t()));
+    } catch (error) {
+      console.error(`remark-ogp-card: ${error}`);
+    }
+
+    return tree;
+  };
+};
+
+export default ogpCardPlugin;
+
+const createCard = (url, domain, title) => {
+  return `
+<div class="not-prose remark-card">
+  <a
+    href="${url}"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h5 class="title">${title}</h5>
+    <div class="site">
+      <img
+        src="https://icons.duckduckgo.com/ip3/${domain}.ico"
+        alt="favicon"
+        class="favicon"
+      />
+      <p class="domain">${domain}</p>
+    </div>
+  </a>
+</div>
+`;
+};
+
+const extractDomain = (url) => {
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.hostname;
+  } catch (error) {
+    return "";
+  }
+};
