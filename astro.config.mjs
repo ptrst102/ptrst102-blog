4fd7e34726b3bf4a5fdc2bbd2a8a487e94b477fd
ptrst102 <ptrst102@gmail.com>
2024-03-30 11:38:38 +0900

    compress-astro svg圧縮のみ無効化

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import compress from "compress-astro";
 import ogpCardPlugin from "./tools/remark-ogp-card";
 
 import react from "@astrojs/react";
@@ -17,7 +18,9 @@ export default defineConfig({
     sitemap(),
     react(),
     icon(),
-    // compress(),
+    compress({
+      SVG: false,
+    }),
   ],
   markdown: {
     remarkPlugins: ["remark-gfm", "remark-breaks", ogpCardPlugin],
